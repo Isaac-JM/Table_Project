0a06da06eb366c0f35f3b096fa7a6215d88606fc
isaac-JM <isaacjm9871@gmail.com>
2022-09-13 15:50:44 +0200

    add reactive form to filters

--- table_project/src/app/views/form/form.component.ts
+++ table_project/src/app/views/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -7,8 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private form: FormBuilder) { }
+  filterForm: FormGroup = this.form.group({
+    client: [''],
+    ref: [''],
+    user: [''],
+    date: [''],
+    type: [''],
+    state: [false],
+    state1: [false],
+    state2: [false],
+    state3: [false],
+    state4: [false],
+    state5: [false],
+    state6: [false],
+  })
+  ngOnInit(): void {
 
-  ngOnInit(): void {}
+  }
+
+  resetClient() {
+    this.filterForm.controls['client'].setValue("");
+  }
+
+  resetRef() {
+    this.filterForm.controls['ref'].setValue("");
+  }
+
+  resetUser() {
+    this.filterForm.controls['user'].setValue("");
+  }
+
+  resetDate() {
+    this.filterForm.controls['date'].setValue("");
+  }
+
+  resetType() {
+    this.filterForm.controls['type'].setValue("");
+  }
+
+
 
 }
